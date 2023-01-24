0cff9517253ffef6aae424f7caf7d21af680cb78
Amal Mahmoud <45540223+amalmahmoud@users.noreply.github.com>
2023-01-24 11:42:32 +0200

    RoutingForCoursesAdded + HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CoursesService } from './Services/courses.service';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
+const appRoute:Routes = [
+  {path:'', component:HomeComponent},
+  {path:'Home',component:HomeComponent},
+  {path:'Courses',component:CoursesComponent},
+
+]
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute),
+
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
